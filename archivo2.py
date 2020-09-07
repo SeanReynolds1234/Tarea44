866c7a5861ac392f5fdf0550bacfdc8dc2f24850
Sean Reynolds <seankarurr@gmail.com>
2020-09-06 22:50:10 -0300

    second commit

---
+++ archivo2.py
@@ -0,0 +1,6 @@
+diccionario = {'nombre1': 'Sebastián', 'apellido1': 'Gálvez', 'Programación1': 7.0, 'Física1': 6.9, 'nombre2': 'Alejandro', 'apellido2': 'Guzmán', 'Programación2': 7.0, 'Física2': 7.0, 'nombre3': 'Pablo', 'apellido3': 'Garrido', 'Programación3': 7.0, 'Física3': 6.7, 'nombre4': 'Diego', 'apellido4': 'Molina', 'Programación4': 7.0, 'Física4': 6.7}
+print(diccionario['nombre1'], diccionario['apellido1']), print('Nota Programación'), print(diccionario['Programación1']), print('Nota Física'), print(diccionario['Física1'])
+print(diccionario['nombre2'], diccionario['apellido2']), print('Nota Programación'), print(diccionario['Programación2']), print('Nota Física'), print(diccionario['Física2'])
+print(diccionario['nombre3'], diccionario['apellido3']), print('Nota Programación'), print(diccionario['Programación3']), print('Nota Física'), print(diccionario['Física3'])
+print(diccionario['nombre4'], diccionario['apellido4']), print('Nota Programación'), print(diccionario['Programación4']), print('Nota Física'), print(diccionario['Física4'])
+
